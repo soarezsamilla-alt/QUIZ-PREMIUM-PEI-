e6305690fecdbcc86df40005fadb200d1c8e3e0e
Samilla Soarez <soarezsamilla@gmail.com>
2026-04-23 22:45:53 +0000

    Agora, eu preciso que você troque a imagem na primeira seção da headline

--- src/components/sales/SalesPage.tsx
+++ src/components/sales/SalesPage.tsx
@@ -85,7 +85,7 @@ export function SalesPage() {
 
           <div className="relative z-10 w-full max-w-[460px] h-[300px] mx-auto mb-2 -mt-4">
             <NextImage 
-              src="https://www.image2url.com/r2/default/images/1776983219615-c8446671-6396-41d4-9d62-8e0b64305c1e.png"
+              src="https://www.image2url.com/r2/default/images/1776984250215-d7ff79b6-d68a-4fa3-a383-417ce868746e.png"
               alt="Visualização do Material PEI"
               fill
               className="object-contain"
@@ -575,7 +575,7 @@ export function SalesPage() {
 
                 <div id="plano-pro-image" className="relative w-full h-[300px] mb-4 scroll-mt-24">
                   <NextImage 
-                    src="https://www.image2url.com/r2/default/images/1776983219615-c8446671-6396-41d4-9d62-8e0b64305c1e.png"
+                    src="https://www.image2url.com/r2/default/images/1776984250215-d7ff79b6-d68a-4fa3-a383-417ce868746e.png"
                     alt="Formas de pagamento"
                     fill
                     className="object-contain"
